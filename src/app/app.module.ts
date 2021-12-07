875c5ccdf8bffc8f366255e0558d15fb9a266217
Andri Okta <andreroger167@gmail.com>
2021-12-07 14:08:20 +0700

    update 7 Desember 2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StandingsComponent } from './standings/standings.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScoringsComponent } from './scorings/scorings.component';
 import { AdminComponent } from './admin/admin.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { AdminComponent } from './admin/admin.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/standings/standings.component.ts
+++ src/app/standings/standings.component.ts
@@ -1,4 +1,12 @@
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { scheduled } from 'rxjs';
+import { Rangking } from '../interface/ranking';
+import { Schedule } from '../interface/schedule';
+import { Team } from '../interface/team';
+import { Teams } from '../service/ScheduleData';
+import { SoccerService } from '../service/Soccer.Service';
 
 @Component({
   selector: 'app-standings',
@@ -6,10 +14,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./standings.component.css']
 })
 export class StandingsComponent implements OnInit {
+  public UsingAsync:boolean = false;
+  public MyTeams : Team[];
+  public LeagueName : String;
+  public MySchedule : Schedule[];
+  public Standings : Rangking[];
 
-  constructor() { }
 
+  constructor(private _titleService:Title, private _soccerService: SoccerService) {
+    this._titleService.setTitle('Pertandingan Sepakbola Negara Akoeh');
+    this.MyTeams=[];
+    this.MySchedule=[];
+    this.Standings=[];
+    this.getTeams();
+    this.LeagueName="Liga Ku";
+    this.getSchedule();
+    this.ComputeRankings();
+  }
   ngOnInit(): void {
   }
+  getTeams(){
+    if(this.UsingAsync){
+      let xx = this._soccerService.getAllTeamsAsync();
+        xx.then((Teams:Team[])=>this.MyTeams = Teams);
+    }
+    else {
+      this.MyTeams = this._soccerService.getAllTeams();
+    }
+  }
 
-}
+  private getSchedule(){
+    if(this.UsingAsync){
+      let xx = this._soccerService.getScheduleAsync();
+        xx.then((Schedules:Schedule[])=> this.MySchedule = Schedules);
+    } else {
+      this.MySchedule = this._soccerService.getSchedule();
+    }
+  }
+  public ComputeRankings(){
+    var curDate: Date =new Date();
+    var TeamAt: number;
+    this.Standings = [];
+    this.MySchedule.forEach(element => {
+      if(element.PlayingDate < curDate && element.HomeScore>=0){
+        TeamAt = this.FindTeam(element.HomeTeam);
+        if(TeamAt < 0) {
+          this.Standings.push({
+            TeamName:element.AwayTeam,
+            GamesPlayed:0,Wins:0,Ties:0,GoalsFor:0,GoalAgaints:0
+          })
+          TeamAt = this.Standings.length-1; 
+        }
+        this.UpdCurrentRow(element,TeamAt,"A")
+      }
+    })
+  }
+  private UpdCurrentRow(element:Schedule,TeamAt:number,HomeAway:string){
+    this.Standings[TeamAt].GamesPlayed++;
+    if(HomeAway=='H'){
+      this.Standings[TeamAt].GoalsFor += element.HomeScore;
+      this.Standings[TeamAt].GoalAgaints += element.AwayScore;
+      //win menang
+      if(element.HomeScore>element.AwayScore){
+        this.Standings[TeamAt].Wins++;
+    }
+  }
+
+    if(HomeAway=='A'){
+      this.Standings[TeamAt].GoalsFor += element.AwayScore;
+      this.Standings[TeamAt].GoalsFor += element.HomeScore;
+      if(element.AwayScore>element.HomeScore){
+        this.Standings[TeamAt].Wins++;
+    }
+  }
+      if(element.HomeScore==element.AwayScore){
+        this.Standings[TeamAt].Ties++;
+    }
+  }
+  private FindTeam(TeamName:string):number {
+    var FoundAt:number =-1;
+    for(var _x=0; _x<this.Standings.length; _x++){
+      if(this.Standings[_x].TeamName==TeamName){
+        return _x;
+      }
+    }
+    return FoundAt;
+  }
+  
+  }
+
